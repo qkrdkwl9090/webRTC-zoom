b64a59e054ba0529e1aa08e1b610f07a1a057dcc
Tony <qkrdkwl9090@naver.com>
2024-01-17 17:17:12 +0900

    feat(express): set view engine using ejs

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));
+
 const handleListen = () => console.log("Hello World!");
 
 app.listen(3000, handleListen);
