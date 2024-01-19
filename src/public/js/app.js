a11e8112f890f807bcf8e43091f4a02c8315c41b
Tony <qkrdkwl9090@naver.com>
2024-01-19 21:30:17 +0900

    feat(main): switch camera

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,18 +3,44 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const cameraSelect = document.getElementById("cameras");
 
 let myStream;
 let muted = false;
 let cameraOff = false;
 
-const getMedia = async () => {
+const getCameras = async () => {
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      if (currentCamera.label === camera.label) option.selected = true;
+      cameraSelect.appendChild(option);
     });
+  } catch (e) {
+    console.error(e);
+  }
+};
+const getMedia = async (deviceId) => {
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
+  const cameraConstrains = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
+
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstrains : initialConstrains
+    );
     myFace.srcObject = myStream;
+    if (!deviceId) await getCameras();
   } catch (e) {
     console.error(e);
   }
@@ -46,8 +72,11 @@ const handleCameraClick = () => {
   }
 };
 
+const handleCameraChange = async () => await getMedia(cameraSelect.value);
+
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
+cameraSelect.addEventListener("input", handleCameraChange);
 
 // const welcome = document.querySelector("#welcome");
 // const form = welcome.querySelector("form");
