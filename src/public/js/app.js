0327c523f79bdba5e59b0f0a914a42d4cf6e6bc0
qkrdkwl9090 <qkrdkwl9090@naver.com>
2024-01-22 15:53:36 +0900

    fix(main): set sender(change camera)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -76,7 +76,16 @@ const handleCameraClick = () => {
   }
 };
 
-const handleCameraChange = async () => await getMedia(cameraSelect.value);
+const handleCameraChange = async () => {
+  await getMedia(cameraSelect.value);
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === "video");
+    videoSender.replaceTrack(videoTrack);
+  }
+};
 
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
